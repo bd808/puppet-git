49100fe26219cfd86af06d6f553973a65f389117
Bryan Davis <bd808@wikimedia.org>
2013-08-14 19:49:51 -0600

    Use "latest" git and no config tweaks.

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,11 +7,7 @@ class git {
   include homebrew
   include git::config
 
-  homebrew::formula { 'git':
-    before => Package['boxen/brews/git'],
-  }
-
-  package { 'boxen/brews/git':
+  package { 'git':
     ensure => $git::config::version
   }
 
@@ -26,33 +22,12 @@ class git {
   file { $git::config::global_credentialhelper:
     ensure  => link,
     target  => $git::config::credentialhelper,
-    before  => Package['boxen/brews/git'],
+    before  => Package['git'],
     require => File[$git::config::credentialhelper]
   }
 
   file { "${git::config::configdir}/gitignore":
     source  => 'puppet:///modules/git/gitignore',
     require => File[$git::config::configdir]
-  }
-
-  git::config::global{ 'credential.helper':
-    value => $git::config::global_credentialhelper
-  }
-
-  git::config::global{ 'core.excludesfile':
-    value   => "${git::config::configdir}/gitignore",
-    require => File["${git::config::configdir}/gitignore"]
-  }
-
-  if $::gname {
-    git::config::global{ 'user.name':
-      value => $::gname
-    }
-  }
-
-  if $::gemail {
-    git::config::global{ 'user.email':
-      value => $::gemail
-    }
   }
 }
